481f6a6d066927390e3cac6de2b5eabafb85f65c
Xagly Montilva <xaglymontilva@gmail.com>
2025-05-15 22:59:20 +0200

    created Past Events Page, pulls all past events from today's date, pagination.  Added link to this page from ALL Events

--- app/public/wp-content/themes/fictional-university-theme/archive-event.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-event.php
@@ -32,6 +32,8 @@ get_header(); ?>
 
     echo paginate_links();
     ?>
+    <hr class="section-break">
+    <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events')?>">Check out out past events archive.</a></p>
 </div>
 
 
