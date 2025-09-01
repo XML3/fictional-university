145b29936540317e2ee8f96d199696c2ac40f099
Xagly Montilva <xaglymontilva@gmail.com>
2025-09-01 20:50:33 +0200

    created conditionals to render information when posts are not found,. Modify the url to flexible - dynamic url.

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '811610f0037985899f1d');
+<?php return array('dependencies' => array('jquery'), 'version' => '7703afc1c31b7e064596');
